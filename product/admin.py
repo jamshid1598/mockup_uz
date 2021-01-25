f4e40f7c16340cc70465f72f00747442fe4b6fe0
jamshid1598 <dovurovjamshid95@gmail.com>
2021-01-25 17:39:25 +0500

    Product database structure is configured

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from django.db.models import TextField
+from tinymce.widgets import TinyMCE
+from .models import (
+    Category,
+    Product,
+    MockUp,
+    Image,
+    Tag,
 
+)
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    #  ('name', 'slug', 'image', 'caption', )
+    list_display  = ('name',)
+    orderinig     = ('name',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+
+    prepopulated_fields = {'slug': ('name',)}
+
+    fieldsets = (
+        ('Category', {
+            "fields": (
+                'name', 'slug', 'image', 'caption'
+            ),
+        }),
+    )
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    # ('category', 'name', 'slug', 'description', 'price', 'discount', 'paid', 'free', 'downloaded', 'viewed', 'liked', 'tags', )
+    list_display = ('category', 'name', 'price', 'discount', 'paid', 'free', 'downloaded', 'viewed',)
+    list_display_links = ('name', 'downloaded', 'viewed')
+    ordering = ('category', 'name', 'price', 'discount', 'paid', 'free', 'downloaded', 'viewed', 'tags')
+    search_fields = ('category', 'name', 'price', 'discount', 'paid', 'free', 'downloaded', 'viewed', 'tags')
+
+    list_editable = ('category', 'price', 'discount', 'paid', 'free', )
+
+    prepopulated_fields = {'slug': ('name',)}
+    
+    fieldsets = (
+        ('Category', {
+            "fields": (
+                'category',
+            ),
+        }),
+        ('MockUp Info', {
+            "fields": (
+                'name', 'slug', 'description', 'tags',
+            )
+        }),
+        ('Price Info', {
+            'fields' : (
+                'price', 'discount', 'paid', 'free',
+            )
+        }),
+    )
+
+    formfield_overrides = {
+        TextField: {'widget': TinyMCE }
+    }    
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from  django.contrib.auth import get_user_model
+User = get_user_model()
+# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=250)
+
+    image = models.ImageField(upload_to="category-image/%d/%m/%Y/")
+    caption = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Tag(models.Model):
+    category = models.ManyToManyField(Category)
+    tag = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category')
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=300)
+    description = models.TextField()
+    price = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)))
+    discount = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)))
+    paid = models.BooleanField(default=True)
+    free = models.BooleanField(default=False) 
+
+    downloaded = models.SmallIntegerField(default=0)
+    viewed = models.SmallIntegerField(default=0)
+
+    liked = models.ManyToManyField(User)
+    tags = models.ManyToManyField(Tag)
+
+    def __str_(self):
+        return self.name + " | " + self.category.name
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+    
+    image = models.ImageField(upload_to="product-image/%d/%m/%Y/")
+    caption = models.CharField(max_length=300)
+
+    def __str__(self):
+        return "image pk: "+(self.pk)+self.product.name
+
+class MockUp(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_file")
+
+    file = models.FileField(upload_to="mockup-file/%d/%m/%Y/")
+    resolution = models.CharField(max_length=50)
+    extension = models.CharField(max_length=10)
+    size = models.CharField(max_length=10)
+
