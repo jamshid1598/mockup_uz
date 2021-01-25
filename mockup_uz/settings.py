c72bb4bb2c6f5c93fd74c56da8a7dc3e2bfa8439
jamshid1598 <dovurovjamshid95@gmail.com>
2021-01-25 18:53:26 +0500

    media_root fixed

--- mockup_uz/settings.py
+++ mockup_uz/settings.py
@@ -156,9 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static_root/'
+STATIC_ROOT = BASE_DIR / 'static_root/'
 
 STATICFILES_DIRS = (BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media_root/'
+MEDIA_ROOT = BASE_DIR / 'media_root/'
