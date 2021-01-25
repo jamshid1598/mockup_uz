538ccbca4e35d3ae1d1c7e8f04987ea2500ae7a5
jamshid1598 <dovurovjamshid95@gmail.com>
2021-01-25 15:37:09 +0500

    new custom user model created

--- mockup_uz/settings.py
+++ mockup_uz/settings.py
@@ -27,6 +27,36 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+#  TinyMCE Configurations
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
+
 
 # Application definition
 
@@ -44,6 +74,8 @@ INSTALLED_APPS = [
     'product.apps.ProductConfig',
     'core.apps.CoreConfig',
 ]
+
+AUTH_USER_MODEL = 'accounts.NewUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mockup_uz/urls.py
+++ mockup_uz/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,8 @@ urlpatterns = [
     path('product/', include('product.urls', namespace="product")),
     path('', include('core.urls', namespace="core")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
