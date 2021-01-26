06706884c8c5fd5d69ab63047649004dadfde705
jamshid1598 <dovurovjamshid95@gmail.com>
2021-01-26 10:49:27 +0500

    database changed

--- product/models.py
+++ product/models.py
@@ -10,11 +10,11 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=250)
+    name = models.CharField(max_length=50, verbose_name="Name")
+    slug = models.SlugField(max_length=250, verbose_name="Slug")
 
-    image = models.ImageField(upload_to="category-image/%d/%m/%Y/")
-    caption = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="category-image/%d/%m/%Y/", blank=True, null=True, verbose_name="Image")
+    caption = models.CharField(max_length=300, blank=True, null=True, verbose_name="Caption")
 
     def __str__(self):
         return self.name
@@ -22,44 +22,46 @@ class Category(models.Model):
 
 
 class Tag(models.Model):
-    category = models.ManyToManyField(Category)
-    tag = models.CharField(max_length=50)
+    category = models.ManyToManyField(Category, verbose_name="Category")
+    tag = models.CharField(max_length=50, verbose_name="Tag Name")
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category')
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL, related_name='product_category', verbose_name="Category")
 
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=300)
-    description = models.TextField()
-    price = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)))
-    discount = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)))
-    paid = models.BooleanField(default=True)
-    free = models.BooleanField(default=False) 
+    name = models.CharField(max_length=200, verbose_name="Name")
+    slug = models.SlugField(max_length=300, verbose_name="Slug")
+    description = models.TextField(verbose_name="Description")
+    price = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)), verbose_name="Price")
+    discount = models.FloatField(blank=True, null=True, validators=(MinValueValidator(0), MaxValueValidator(100000000)), verbose_name="Discount")
+    paid = models.BooleanField(default=True, verbose_name="Paid")
+    free = models.BooleanField(default=False, verbose_name="Free") 
 
     downloaded = models.SmallIntegerField(default=0)
     viewed = models.SmallIntegerField(default=0)
 
-    liked = models.ManyToManyField(User)
-    tags = models.ManyToManyField(Tag)
+    liked = models.ManyToManyField(User, related_name="user_likes",)
+    tags = models.ManyToManyField(Tag, related_name="product_tags", verbose_name="Product Tags")
 
     def __str_(self):
         return self.name + " | " + self.category.name
 
 class Image(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image", verbose_name="Product")
     
-    image = models.ImageField(upload_to="product-image/%d/%m/%Y/")
-    caption = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="product-image/%d/%m/%Y/", verbose_name="Image")
+    caption = models.CharField(max_length=300, blank=True, null=True, verbose_name="Caption")
 
     def __str__(self):
         return "image pk: "+(self.pk)+self.product.name
 
 class MockUp(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_file")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_file", verbose_name="Product")
 
-    file = models.FileField(upload_to="mockup-file/%d/%m/%Y/")
-    resolution = models.CharField(max_length=50)
-    extension = models.CharField(max_length=10)
-    size = models.CharField(max_length=10)
+    file = models.FileField(upload_to="mockup-file/%d/%m/%Y/", verbose_name="MockUp File")
+    resolution = models.CharField(max_length=50, blank=True, null=True, verbose_name="Resolution")
+    extension = models.CharField(max_length=10, blank=True, null=True, verbose_name="Extension")
+    size = models.CharField(max_length=10, blank=True, null=True, verbose_name="Size")
 
+    def __str__(self):
+        return "File pk: "+str(self.pk)+self.product.name
