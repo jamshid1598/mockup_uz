fb97600409770e9d59a55e893661f4f5222c3908
jamshid1598 <dovurovjamshid95@gmail.com>
2021-05-04 18:25:49 +0500

    admin url configured

--- mockup_uz/urls.py
+++ mockup_uz/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mockup.uz/admin/page/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
 
     path('', include('core.urls', namespace="core")),
